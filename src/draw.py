e634d53b79d72d98f0dde7e4c7ae22ca6994ccea
habib1234731 <habibrehman1234731@gmail.com>
2018-06-20 14:23:05 -0400

    edges starting from the first vertex..?

--- src/draw.py
+++ src/draw.py
@@ -14,9 +14,13 @@ print(graph_data.vertices)
 N = 9
 node_indices = list(range(N))
 
-debug_pallete = Spectral8
-debug_pallete.append('#ff0000')
-debug_pallete.append('#ffff00')
+# debug_pallete = Spectral8
+# debug_pallete.append('#ff0000')
+# debug_pallete.append('#0000ff')
+
+color_list = []
+for vertex in graph_data.vertices:
+    color_list.append(vertex.color)
 
 plot = figure(title='Graph Layout Demonstration', x_range=(-500,500), y_range=(-500,500),
               tools='', toolbar_location=None)
@@ -24,18 +28,27 @@ plot = figure(title='Graph Layout Demonstration', x_range=(-500,500), y_range=(-
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_pallete, 'color')
+#changed debug_palette to color_list
+graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=20, width=40, fill_color='color')
 
+### this is drawing the edges from start to end
+
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=[0]*N, #this is why all the edges start from the first vertex
+                #and is a list of some kind that has to do with starting points
+    end=node_indices) #this is a list of some kind that has to do with ending points
 
 ### start of layout code
-### Looks like this is setting the positions of the vertices
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+### This is setting the positions of the vertices
+# circ = [i*2*math.pi/8 for i in node_indices]
+# x = [math.cos(i) for i in circ] or [... for v in graph.vertices]
+# y = [math.sin(i) for i in circ]
+x = [v.pos['x'] for v in graph_data.vertices]
+y = [v.pos['y'] for v in graph_data.vertices]
+
+print('x is ',x)
+print('y is ',y)
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
--- src/graph.py
+++ src/graph.py
@@ -15,8 +15,13 @@ class Graph:
     def debug_create_test_data(self):
         debug_vertex_1 = Vertex('t1', x = 40, y = 40)
         debug_vertex_2 = Vertex('t2', x = 140, y = 140)
+        debug_vertex_3 = Vertex('t3', x = 300, y = 400)
         # print(debug_vertex_1.pos['x'])
+        
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
 
-        self.vertices.extend([debug_vertex_1, debug_vertex_2])
+        debug_edge_2 = Edge(debug_vertex_2)
+        debug_vertex_3.edges.append(debug_edge_2)
+
+        self.vertices.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3])
